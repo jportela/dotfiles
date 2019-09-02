209c76c44772ab06eb6c2bd8201e311083f2b7c9
Joao Portela <joao@invisionapp.com>
2019-09-02 16:46:52 +0100

    chmod execs

--- term/oh-my-zsh.sh
+++ term/oh-my-zsh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ln -s "$(pwd)/term/.zshrc" ~/.zshrc
